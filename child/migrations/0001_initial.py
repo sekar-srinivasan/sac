c4174574ee56d321e6913025d7d88f3c5d4b32a2
Sekar Srinivasan <sekar.srinivasan@gmail.com>
2018-11-05 16:53:56 -0800

    renamed initial migration file for project to remove conflict with AWS version

--- child/migrations/0001_initial.py
+++ child/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('project', '0001_initial'),
+        ('project', '0002_initial'),
     ]
 
     operations = [
